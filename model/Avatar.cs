42eb7e010d05ce33aa46c27da264b2ab25ee8f2d
MertErgun <mert.rgun@gmail.com>
2016-02-15 22:21:28 +0100

    commit missing files

---
+++ model/Avatar.cs
@@ -0,0 +1,68 @@
+﻿using MYMGames.Hopscotch.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace MYMGames.Hopscotch.Model
+{
+    // The model of the avatar view
+    class Avatar
+    {
+        public Brush borderColor { get; set; }
+        public string avatar_id { get; set; }
+        private bool _active;
+        public bool active
+        {
+            get
+            {
+                return _active;
+            }
+            set
+            {
+                _active = value;
+                if (_active) borderColor = new SolidColorBrush(Colors.Green);
+                else borderColor = new SolidColorBrush(Colors.Transparent);
+            }
+        }
+        private Uri avatar_path;
+        private ImageSource _avatar = null;
+        public ImageSource avatar
+        {
+            get
+            {
+                if (_avatar == null && avatar_path != null)
+                {
+                    _avatar = new BitmapImage(avatar_path);
+                }
+
+                return _avatar;
+            }
+
+            private set { }
+        }
+        private RelayCommand _command;
+        public RelayCommand command
+        {
+            get
+            {
+                return _command;
+            }
+            set
+            {
+                _command = value;
+            }
+        } 
+
+        public Avatar(string avatar_id, RelayCommand command, int player_index)
+        {
+            avatar_path = new Uri("../Images/avatars/avatar" + avatar_id + ".jpg", UriKind.Relative);
+            borderColor = new SolidColorBrush(Colors.Transparent);
+            _command = command;
+            this.avatar_id = player_index + " " + avatar_id;
+        }
+    }
+}
---
+++ model/PlayerAvatar.cs
@@ -0,0 +1,46 @@
+﻿using MYMGames.Hopscotch.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace MYMGames.Hopscotch.Model
+{
+    class PlayerAvatar : Player
+    {
+        public bool active { get; set; }
+        public bool isEnabled { get; set; }
+        public int selected_avatar_index;
+        private List<Avatar> _avatars;
+        public List<Avatar> avatars
+        {
+            get
+            {
+                return _avatars;
+            }
+            set
+            {
+                _avatars = value;
+            }
+        }
+        public PlayerAvatar(string avatar_path, RelayCommand command, int player_index) : base(avatar_path)
+        {
+            _avatars = AvatarFactory.getListOfAvatars(command, player_index);
+            _avatars.First().active = true;
+            selected_avatar_index = 0;
+            isEnabled = true;
+            active = false;
+        }
+
+        public string getSelectedAvatar()
+        {
+            foreach (Avatar avatar in avatars)
+            {
+                if (avatar.active) return "../Images/avatars/avatar" + avatar.avatar_id.Substring(2) + ".jpg";
+            }
+            return null;
+        }
+    }
+}
